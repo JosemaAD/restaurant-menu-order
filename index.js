96670439faa3df5fdf3a870d2485d5b7d9583927
Josema AD <josemanuelarandadelgado@gmail.com>
2023-06-04 12:36:36 +0200

    All functionalities added
    Payment functionality added
    Success message added
    Some refactoring
    Commentaries for documentation purpose.

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
 import { menuArray } from './data.js';
-import { v4 as uuidv4 } from 'https://jspm.dev/uuid';
-let cart = []
 
 const foodMenuList = document.getElementById('food-menu-list')
 const orderDetails = document.getElementById('order-details')
@@ -8,7 +6,10 @@ const completeOrderBtn = document.getElementById('complete-order-btn')
 const checkoutForm = document.getElementById('checkout-form')
 const totalPrice = document.getElementById('total-price')
 const payOrderBtn = document.getElementById('pay-order-btn')
+const cartSummary = document.getElementById('cart-summary')
+let cart = []
 
+// Event listener for add to cart and remove from cart
 document.addEventListener("click", function(e){
     if(e.target.dataset.add){
         handleAddToCartBtn(e.target.dataset.add)
@@ -18,42 +19,52 @@ document.addEventListener("click", function(e){
     }
 })
 
+// event listener for complete order button
 completeOrderBtn.addEventListener('click', function(){
     if(cart.length > 0){
         checkoutForm.classList.remove('d-none')
-    }else{
-        console.log('Cart is empty!')
     }
 })
 
-payOrderBtn.addEventListener('click', function(){
+// Event listener for payment and order completed state
+payOrderBtn.addEventListener('click', function(e){
     if(document.getElementById('name-checkout').value && document.getElementById('card-number-checkout').value && document.getElementById('card-cvv-checkout').value){
-        console.log('hey')
+        e.preventDefault()
+        setTimeout(function(){
+            cartSummary.classList.add('d-none')
+            checkoutForm.classList.add('d-none')
+            cartSummary.textContent = `Thanks ${document.getElementById('name-checkout').value} Your order is on its way!w`
+            cartSummary.classList.toggle('d-none')
+        }, 2000);
     }
 })
 
+// Handle add to cart array
 function handleAddToCartBtn(foodId){
     const itemAdded = menuArray.filter(function(food){
         return food.uuid === foodId
     })[0]
 
     cart.push(itemAdded) 
-
+    completeOrderBtn.disabled = false
     renderCart(cart)
 }
 
+//Handle remove from cart array
 function handleRemoveFromCartBtn(foodId){
-
     const itemRemoved = cart.filter(function(food){
         return food.uuid === foodId
     })[0]
 
     let indexOfFood = cart.indexOf(itemRemoved)
     cart.splice(indexOfFood, 1)
-
+    if(cart.length === 0){
+        completeOrderBtn.disabled = true
+    }
     renderCart(cart)
 }
 
+// Print on UI the carts items 
 function cartItems(cart){
     let cartHtml = ``
     if(cart.length > 0){
@@ -78,6 +89,7 @@ function cartItems(cart){
     return cartHtml
 }
 
+// Manage and print on UI the total cart
 function cartTotal(cart){
     let cartTotalHtml = ``
     let cartTotalPrice = 0
@@ -96,6 +108,7 @@ function cartTotal(cart){
     return cartTotalHtml
 }
 
+// Print menu on UI
 function getFeedHtml(){
     let htmlMenu = ``
     menuArray.forEach(function(food){   
@@ -118,10 +131,12 @@ function getFeedHtml(){
     return htmlMenu
 }
 
+// Render menu function
 function renderMenu(){
     foodMenuList.innerHTML = getFeedHtml()
 }
 
+// Render cart function
 function renderCart(cart){
     if(cart){
         orderDetails.innerHTML = cartItems(cart)
